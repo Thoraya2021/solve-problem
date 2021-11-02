50c1e59b656bd4e0930a145650a7592afefb4295
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-02 20:28:30 +0300

    solve

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,12 @@
-const fizzbuzz =(n)=>
-{
-let num=1;
-let printnum="";
-
-if (num < 10) {
-  printnum = "";
-} else if (num < 20) {
-  printnum = "";
-} else {
-  printnum= "";
-}
 
 
-};
+  const fizzBuzz = (num) => {
+    for(let i = 1; i <= num; i++) {
+      if(i % 15 === 0) console.log('FizzBuzz');
+      else if(i % 3 === 0) console.log('Fizz');
+      else if(i % 5 === 0) console.log('Buzz');
+      else console.log(i)
+    }
+  }
+   
+  fizzBuzz()
