4efeda1c8bbb9bebb4daefc318251654a80ed6b7
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-15 21:48:05 +0300

    not finish

---
+++ isprime.js
@@ -0,0 +1,20 @@
+function primes(start,end)
+
+{
+  const prime=numuber=>{
+    
+    for (let i=2;i<number;i++)
+      {
+        if (number%i===0)
+          return null;
+        
+      }
+
+    return number !== 1;
+    
+  };
+  return primes.filter(start,end);
+
+  
+
+}
